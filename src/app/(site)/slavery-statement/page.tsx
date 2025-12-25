a617103ac13b036551c24836e6b91edadd67dae6
Shubham Chaudhary <sc3617378@gmail.com>
2025-12-25 15:55:18 +0530

    added insta link

--- src/app/(site)/slavery-statement/page.tsx
+++
@@ -1,9 +0,0 @@
-import ModernSlaveryStatementPage from "@/components/sections/SlaveryStatementPageWrapper";
-export default function SlaveryStatementPageWrapper() {
-
-    return (
-        <>
-            <ModernSlaveryStatementPage />
-        </>
-    );
-}
